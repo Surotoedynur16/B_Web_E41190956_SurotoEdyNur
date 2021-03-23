5d4ea49b65a5906735b12cba464e1548111f5775
Surotoedynur16 <71973255+Surotoedynur16@users.noreply.github.com>
2021-03-23 15:11:45 +0700

    Update web.php

--- minggu ke 3/Belajarlaravel/routes/web.php
+++ minggu ke 3/Belajarlaravel/routes/web.php
@@ -17,6 +17,14 @@ use App\Http\Controllers\ManagementUserController;
 Route::get('user', [ManagementUserController::class, 'index']);
 Route::post('user', [ManagementUserController::class, 'store']);
 Route::get('user/create', [ManagementUserController::class, 'create']);
+Route::get('user/{id}', [ManagementUserController::class, 'show']);
+Route::put('user/{id}', [ManagementUserController::class, 'update']);
+Route::delete('user/{id}', [ManagementUserController::class, 'destroy']);
+Route::get('user/{id}/edit', [ManagementUserController::class, 'edit']);
+
+
+
+
 
 
 
