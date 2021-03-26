461eebc53c48ed82e24618f0d974923cad133f77
Surotoedynur16 <71973255+Surotoedynur16@users.noreply.github.com>
2021-03-26 07:43:22 +0700

    Menambahkan routes

--- minggu3_tugas3/Belajar_Laravel2/routes/web.php
+++ minggu3_tugas3/Belajar_Laravel2/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ManagementUserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/home', function () {
-    return view('home');
-});
+
+
+Route::get('user', [ManagementUserController::class, 'index']);
