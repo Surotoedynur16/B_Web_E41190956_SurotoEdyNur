fe5d411eed5392b494b967297ba3b10ef63dae24
Surotoedynur16 <71973255+Surotoedynur16@users.noreply.github.com>
2021-03-26 07:41:29 +0700

    menambahkan Route dan memanggil variabel

--- minggu3_tugas3/Belajar_Laravel2/app/Http/Controllers/ManagementUserController.php
+++ minggu3_tugas3/Belajar_Laravel2/app/Http/Controllers/ManagementUserController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class ManagementUserController extends Controller
 {
-    //
+    public function index()
+    {
+        $nama = "Suroto Edy Nur";
+
+        $pelajaran = ["Algoritma & Pemograman", "kalkulus", "Pemograman Web"];
+
+        return view('home', compact('nama', 'pelajaran'));
+    }
 }
